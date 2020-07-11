35b84f79ecec09465cad24762bd9c0d0c0d96b74
JungDefiant <jungdefiant@gmail.com>
2020-07-11 15:31:36 -0700

    Changes to game loop

--- Lab 04 Tic Tac Toe Round 2/Classes/Game.cs
+++ Lab 04 Tic Tac Toe Round 2/Classes/Game.cs
@@ -29,41 +29,14 @@ namespace Lab04_TicTacToe.Classes
 		/// <returns>Winner</returns>
 		public Player Play()
 		{
-
-			//TODO: Complete this method and utilize the rest of the class structure to play the game.
-
-			/*
-             * Complete this method by constructing the logic for the actual playing of Tic Tac Toe. 
-             * 
-             * A few things to get you started:
-            1. A turn consists of a player picking a position on the board with their designated marker. 
-            2. Display the board after every turn to show the most up to date state of the game
-            3. Once a Winner is determined, display the board one final time and return a winner
-
-            Few additional hints:
-                Be sure to keep track of the number of turns that have been taken to determine if a draw is required
-                and make sure that the game continues while there are unmarked spots on the board. 
-
-            Use any and all pre-existing methods in this program to help construct the method logic. 
-             */
-
-			bool winnerFound = false;
-
-			while(!winnerFound)
+			while(true)
             {
 				Board.DisplayBoard();
 
-				// Player inputs position on board (need to check if it's a valid position)
-				// Player's marker is put on the board
-				// Game checks for winner
-				// If winner, then return current player
-				// If no winner, then switch player
-
-				// string input = Console.ReadLine();
 				NextPlayer().TakeTurn(Board);
-				winnerFound = CheckForWinner(Board);
-				if (winnerFound) // if method returns 'true'
+				if (CheckForWinner(Board))
 				{
+					Board.DisplayBoard();
 					Winner = NextPlayer();
 					return Winner;
 				}
@@ -72,8 +45,6 @@ namespace Lab04_TicTacToe.Classes
 					SwitchPlayer();
 				}
 			}
-
-			return null;
 		}
 
 
